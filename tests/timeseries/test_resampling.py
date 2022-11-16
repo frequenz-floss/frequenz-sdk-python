e2136d113708fcf6cb9f108fd0aed9318a92a335
Leandro Lucarella <leandro.lucarella@frequenz.com>
2022-11-16 10:28:08 +0100

    Use more intuitive variable names in resampler tests
    With these names it is easier to identify which sample belongs to each
    metric.
    
    Signed-off-by: Leandro Lucarella <leandro.lucarella@frequenz.com>

--- tests/timeseries/test_resampling.py
+++ tests/timeseries/test_resampling.py
@@ -133,25 +133,25 @@ def test_component_metric_group_resampler() -> None:
 
     timestamp = datetime.now(timezone.utc)
 
-    value1 = 5.0
-    value2 = 15.0
-    value3 = 100.0
-    value4 = 999.0
+    value11 = 5.0
+    value12 = 15.0
+    value21 = 100.0
+    value22 = 999.0
 
-    sample1 = Sample(timestamp - timedelta(seconds=0.7), value=value1)
-    sample2 = Sample(timestamp - timedelta(seconds=0.5), value=value2)
-    sample3 = Sample(timestamp - timedelta(seconds=5.05), value=value3)
-    sample4 = Sample(timestamp - timedelta(seconds=0.99), value=value4)
+    sample11 = Sample(timestamp - timedelta(seconds=0.7), value=value11)
+    sample12 = Sample(timestamp - timedelta(seconds=0.5), value=value12)
+    sample21 = Sample(timestamp - timedelta(seconds=5.05), value=value21)
+    sample22 = Sample(timestamp - timedelta(seconds=0.99), value=value22)
 
-    resampler.add_sample(time_series_id=time_series_id_1, sample=sample1)
-    resampler.add_sample(time_series_id=time_series_id_1, sample=sample2)
-    resampler.add_sample(time_series_id=time_series_id_2, sample=sample3)
-    resampler.add_sample(time_series_id=time_series_id_2, sample=sample4)
+    resampler.add_sample(time_series_id=time_series_id_1, sample=sample11)
+    resampler.add_sample(time_series_id=time_series_id_1, sample=sample12)
+    resampler.add_sample(time_series_id=time_series_id_2, sample=sample21)
+    resampler.add_sample(time_series_id=time_series_id_2, sample=sample22)
 
     resampled_samples = dict(resampler.resample())
 
     assert resampled_samples[time_series_id_1].timestamp >= timestamp
-    assert resampled_samples[time_series_id_1].value == sum([value1, value2])
+    assert resampled_samples[time_series_id_1].value == sum([value11, value12])
 
     assert resampled_samples[time_series_id_2].timestamp >= timestamp
-    assert resampled_samples[time_series_id_2].value == value4
+    assert resampled_samples[time_series_id_2].value == value22
