344bd23e875399ce2155e9e1e5367e57429e4d0f
Leandro Lucarella <leandro.lucarella@frequenz.com>
2022-11-17 11:21:18 +0100

    Properly stop server in test if an assertion fails
    If an assertion is raised and we don't properly stop the mock server,
    the test stalls waiting for the server to finish.
    
    Signed-off-by: Leandro Lucarella <leandro.lucarella@frequenz.com>

--- tests/actor/test_resampling.py
+++ tests/actor/test_resampling.py
@@ -61,80 +61,83 @@ async def test_component_metrics_resampling_actor(mocker: MockerFixture) -> None
     server = mock_api.MockGrpcServer(servicer, port=57899)
     await server.start()
 
-    servicer.add_component(1, microgrid_pb2.ComponentCategory.COMPONENT_CATEGORY_GRID)
-    servicer.add_component(
-        3, microgrid_pb2.ComponentCategory.COMPONENT_CATEGORY_JUNCTION
-    )
-    servicer.add_component(4, microgrid_pb2.ComponentCategory.COMPONENT_CATEGORY_METER)
-    servicer.add_component(7, microgrid_pb2.ComponentCategory.COMPONENT_CATEGORY_METER)
-    servicer.add_component(
-        8, microgrid_pb2.ComponentCategory.COMPONENT_CATEGORY_INVERTER
-    )
-    servicer.add_component(
-        9, microgrid_pb2.ComponentCategory.COMPONENT_CATEGORY_BATTERY
-    )
+    try:
 
-    servicer.add_connection(1, 3)
-    servicer.add_connection(3, 4)
-    servicer.add_connection(3, 7)
-    servicer.add_connection(7, 8)
-    servicer.add_connection(8, 9)
+        servicer.add_component(1, microgrid_pb2.ComponentCategory.COMPONENT_CATEGORY_GRID)
+        servicer.add_component(
+            3, microgrid_pb2.ComponentCategory.COMPONENT_CATEGORY_JUNCTION
+        )
+        servicer.add_component(4, microgrid_pb2.ComponentCategory.COMPONENT_CATEGORY_METER)
+        servicer.add_component(7, microgrid_pb2.ComponentCategory.COMPONENT_CATEGORY_METER)
+        servicer.add_component(
+            8, microgrid_pb2.ComponentCategory.COMPONENT_CATEGORY_INVERTER
+        )
+        servicer.add_component(
+            9, microgrid_pb2.ComponentCategory.COMPONENT_CATEGORY_BATTERY
+        )
 
-    await microgrid_api.initialize("[::1]", 57899)
+        servicer.add_connection(1, 3)
+        servicer.add_connection(3, 4)
+        servicer.add_connection(3, 7)
+        servicer.add_connection(7, 8)
+        servicer.add_connection(8, 9)
 
-    channel_registry = ChannelRegistry(name="Microgrid Channel Registry")
+        await microgrid_api.initialize("[::1]", 57899)
 
-    data_source_request_channel = Broadcast[ComponentMetricRequest](
-        "Data Source Request Channel"
-    )
-    data_source_request_sender = data_source_request_channel.get_sender()
-    data_source_request_receiver = data_source_request_channel.get_receiver()
+        channel_registry = ChannelRegistry(name="Microgrid Channel Registry")
 
-    resampling_actor_request_channel = Broadcast[ComponentMetricRequest](
-        "Resampling Actor Request Channel"
-    )
-    resampling_actor_request_sender = resampling_actor_request_channel.get_sender()
-    resampling_actor_request_receiver = resampling_actor_request_channel.get_receiver()
+        data_source_request_channel = Broadcast[ComponentMetricRequest](
+            "Data Source Request Channel"
+        )
+        data_source_request_sender = data_source_request_channel.get_sender()
+        data_source_request_receiver = data_source_request_channel.get_receiver()
 
-    DataSourcingActor(
-        request_receiver=data_source_request_receiver, registry=channel_registry
-    )
+        resampling_actor_request_channel = Broadcast[ComponentMetricRequest](
+            "Resampling Actor Request Channel"
+        )
+        resampling_actor_request_sender = resampling_actor_request_channel.get_sender()
+        resampling_actor_request_receiver = resampling_actor_request_channel.get_receiver()
 
-    ComponentMetricsResamplingActor(
-        channel_registry=channel_registry,
-        subscription_sender=data_source_request_sender,
-        subscription_receiver=resampling_actor_request_receiver,
-        resampling_period_s=0.1,
-    )
+        DataSourcingActor(
+            request_receiver=data_source_request_receiver, registry=channel_registry
+        )
 
-    subscription_request = ComponentMetricRequest(
-        namespace="Resampling",
-        component_id=9,
-        metric_id=ComponentMetricId.SOC,
-        start_time=None,
-    )
+        ComponentMetricsResamplingActor(
+            channel_registry=channel_registry,
+            subscription_sender=data_source_request_sender,
+            subscription_receiver=resampling_actor_request_receiver,
+            resampling_period_s=0.1,
+        )
 
-    await resampling_actor_request_sender.send(subscription_request)
+        subscription_request = ComponentMetricRequest(
+            namespace="Resampling",
+            component_id=9,
+            metric_id=ComponentMetricId.SOC,
+            start_time=None,
+        )
 
-    index = 0
-    expected_sample_values = [
-        3.0,
-        4.5,
-        6.0,
-        7.5,
-        9.0,
-        None,
-        None,
-        None,
-    ]
+        await resampling_actor_request_sender.send(subscription_request)
 
-    async for sample in channel_registry.get_receiver(
-        subscription_request.get_channel_name()
-    ):
-        assert sample.value == expected_sample_values[index]
-        index += 1
-        if index >= len(expected_sample_values):
-            break
+        index = 0
+        expected_sample_values = [
+            3.0,
+            4.5,
+            6.0,
+            7.5,
+            9.0,
+            None,
+            None,
+            None,
+        ]
 
-    await server.stop(0.1)
-    microgrid_api._MICROGRID_API = None  # pylint: disable=protected-access
+        async for sample in channel_registry.get_receiver(
+            subscription_request.get_channel_name()
+        ):
+            assert sample.value == expected_sample_values[index]
+            index += 1
+            if index >= len(expected_sample_values):
+                break
+
+    finally:
+        await server.stop(0.1)
+        microgrid_api._MICROGRID_API = None  # pylint: disable=protected-access
