d43d9fbbd1e1d4a3dace9a1fce16480dc2d4d6a5
Sahas Subramanian <sahas.subramanian@proton.me>
2023-02-01 15:46:55 +0100

    Raise `RuntimeError` instead of raising `Exception`
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- noxfile.py
+++ noxfile.py
@@ -60,7 +60,7 @@ def min_dependencies() -> List[str]:
     """Extract the minimum dependencies from pyproject.toml.
 
     Raises:
-        Exception: If minimun dependencies are not properly
+        RuntimeError: If minimun dependencies are not properly
             set in pyproject.toml.
 
     Returns:
@@ -72,7 +72,7 @@ def min_dependencies() -> List[str]:
 
     dependencies = data.get("project", {}).get("dependencies", {})
     if not dependencies:
-        raise Exception(f"No dependencies found in file: {toml_file.name}")
+        raise RuntimeError(f"No dependencies found in file: {toml_file.name}")
 
     min_deps: List[str] = []
     for dep in dependencies:
@@ -80,7 +80,7 @@ def min_dependencies() -> List[str]:
         if any(op in min_dep for op in (">=", "==")):
             min_deps.append(min_dep.replace(">=", "=="))
         else:
-            raise Exception(f"Minimum requirement is not set: {dep}")
+            raise RuntimeError(f"Minimum requirement is not set: {dep}")
     return min_deps
 
 
