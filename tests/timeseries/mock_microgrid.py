c2387567b338f4867fd4cff732bc3ba53579d79a
Sahas Subramanian <sahas.subramanian@proton.me>
2023-03-19 15:46:24 +0100

    Update `MockMicrogrid` to use `DataPipeline`
    ... instead of starting data pipeline actors directly.
    
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- tests/timeseries/mock_microgrid.py
+++ tests/timeseries/mock_microgrid.py
@@ -8,20 +8,14 @@ from __future__ import annotations
 import asyncio
 import typing
 from datetime import datetime, timezone
-from typing import Callable, Set, Tuple
+from typing import Callable, Set
 
-from frequenz.channels import Broadcast
 from pytest_mock import MockerFixture
 
 from frequenz.sdk import microgrid
 from frequenz.sdk._internal.asyncio import cancel_and_await
-from frequenz.sdk.actor import (
-    ChannelRegistry,
-    ComponentMetricRequest,
-    ComponentMetricsResamplingActor,
-    DataSourcingActor,
-    ResamplerConfig,
-)
+from frequenz.sdk.actor import ResamplerConfig
+from frequenz.sdk.microgrid import _data_pipeline
 from frequenz.sdk.microgrid.client import Connection
 from frequenz.sdk.microgrid.component import (
     Component,
@@ -91,7 +85,6 @@ class MockMicrogrid:  # pylint: disable=too-many-instance-attributes
 
         self._streaming_coros: list[typing.Coroutine[None, None, None]] = []
         self._streaming_tasks: list[asyncio.Task[None]] = []
-        self._actors: list[typing.Any] = []
         self._start_meter_streaming(4)
 
     def start_mock_client(
@@ -119,20 +112,16 @@ class MockMicrogrid:  # pylint: disable=too-many-instance-attributes
         ]
         return self._microgrid
 
-    async def start(
-        self, mocker: MockerFixture
-    ) -> Tuple[Broadcast[ComponentMetricRequest], ChannelRegistry]:
-        """Start the MockServer, and the data source and resampling actors.
-
-        Returns:
-            A sender to send requests to the Resampling actor, and a corresponding
-                channel registry.
-        """
+    async def start(self, mocker: MockerFixture) -> None:
+        """Start the MockServer, and the data source and resampling actors."""
         self.start_mock_client(lambda mock_client: mock_client.initialize(mocker))
-
         await asyncio.sleep(self._sample_rate_s / 2)
-        ret = await self._init_client_and_actors()
-        return ret
+
+        # pylint: disable=protected-access
+        _data_pipeline._DATA_PIPELINE = _data_pipeline._DataPipeline(
+            ResamplerConfig(resampling_period_s=self._sample_rate_s)
+        )
+        # pylint: enable=protected-access
 
     async def _comp_data_send_task(
         self, comp_id: int, make_comp_data: Callable[[int, datetime], ComponentData]
@@ -292,46 +281,11 @@ class MockMicrogrid:  # pylint: disable=too-many-instance-attributes
             self._start_ev_charger_streaming(evc_id)
             self._connections.add(Connection(self._connect_to, evc_id))
 
-    async def _init_client_and_actors(
-        self,
-    ) -> Tuple[Broadcast[ComponentMetricRequest], ChannelRegistry]:
-        channel_registry = ChannelRegistry(name="Microgrid Channel Registry")
-
-        data_source_request_channel = Broadcast[ComponentMetricRequest](
-            "Data Source Request Channel"
-        )
-        data_source_request_sender = data_source_request_channel.new_sender()
-        data_source_request_receiver = data_source_request_channel.new_receiver()
-
-        resampling_actor_request_channel = Broadcast[ComponentMetricRequest](
-            "Resampling Actor Request Channel"
-        )
-        resampling_actor_request_receiver = (
-            resampling_actor_request_channel.new_receiver()
-        )
-
-        self._actors.append(
-            DataSourcingActor(
-                request_receiver=data_source_request_receiver, registry=channel_registry
-            )
-        )
-
-        self._actors.append(
-            ComponentMetricsResamplingActor(
-                channel_registry=channel_registry,
-                data_sourcing_request_sender=data_source_request_sender,
-                resampling_request_receiver=resampling_actor_request_receiver,
-                config=ResamplerConfig(resampling_period_s=self._sample_rate_s),
-            )
-        )
-
-        return (resampling_actor_request_channel, channel_registry)
-
     async def cleanup(self) -> None:
         """Clean up after a test."""
         # pylint: disable=protected-access
-        for actor in self._actors:
-            await actor._stop()
+        if _data_pipeline._DATA_PIPELINE:
+            await _data_pipeline._DATA_PIPELINE._stop()
 
         for task in self._streaming_tasks:
             await cancel_and_await(task)
