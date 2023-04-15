948602eafafdc28e1237786161de09d697732983
Sahas Subramanian <sahas.subramanian@frequenz.com>
2023-04-15 23:13:20 +0200

    Update `examples/battery_pool.py` to use `microgrid.battery_pool()`
    Signed-off-by: Sahas Subramanian <sahas.subramanian@frequenz.com>

--- examples/battery_pool.py
+++ examples/battery_pool.py
@@ -7,69 +7,16 @@ from __future__ import annotations
 
 import asyncio
 import logging
-from datetime import timedelta
 from typing import Any, Dict
 
-from frequenz.channels import Broadcast, Receiver
+from frequenz.channels import Receiver
 from frequenz.channels.util import MergeNamed
 
 from frequenz.sdk import microgrid
-from frequenz.sdk.actor import (
-    ChannelRegistry,
-    ComponentMetricRequest,
-    DataSourcingActor,
-    ResamplerConfig,
-)
-from frequenz.sdk.actor.power_distributing import PowerDistributingActor
-from frequenz.sdk.actor.power_distributing._battery_pool_status import BatteryStatus
-from frequenz.sdk.microgrid import connection_manager
-from frequenz.sdk.microgrid.component import ComponentCategory
-from frequenz.sdk.timeseries.battery_pool import BatteryPool
+from frequenz.sdk.actor import ResamplerConfig
 
 HOST = "microgrid.sandbox.api.frequenz.io"  # it should be the host name.
 PORT = 61060
-
-
-def create_battery_pool() -> BatteryPool:
-    """Create battery pool.
-
-    It needs many instance to be created before.
-
-    Returns:
-        BatteryPool instance ready to use.
-    """
-    channel_registry = ChannelRegistry(name="data-registry")
-
-    # Create a channels for sending/receiving subscription requests
-    data_source_request_channel = Broadcast[ComponentMetricRequest](
-        "data-source", resend_latest=True
-    )
-
-    # Instantiate a data sourcing actor
-    _ = DataSourcingActor(
-        request_receiver=data_source_request_channel.new_receiver(
-            "data_sourcing_receiver"
-        ),
-        registry=channel_registry,
-    )
-
-    battery_status_channel = Broadcast[BatteryStatus]("batteries-status")
-    _ = PowerDistributingActor(
-        users_channels={},
-        battery_status_sender=battery_status_channel.new_sender(),
-    )
-
-    batteries = connection_manager.get().component_graph.components(
-        component_category={ComponentCategory.BATTERY}
-    )
-
-    return BatteryPool(
-        batteries_id=set(battery.component_id for battery in batteries),
-        batteries_status_receiver=battery_status_channel.new_receiver(
-            name="battery_pool", maxsize=1
-        ),
-        min_update_interval=timedelta(seconds=0.2),
-    )
 
 
 async def main() -> None:
@@ -81,7 +28,7 @@ async def main() -> None:
         host=HOST, port=PORT, resampler_config=ResamplerConfig(resampling_period_s=1.0)
     )
 
-    battery_pool = create_battery_pool()
+    battery_pool = microgrid.battery_pool()
     receivers: Dict[str, Receiver[Any]] = {
         "soc": await battery_pool.soc(maxsize=1),
         "capacity": await battery_pool.capacity(maxsize=1),
