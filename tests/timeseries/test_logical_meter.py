1f00d5aaaae9a35f8513219461149d44ea3eff10
Sahas Subramanian <sahas.subramanian@frequenz.com>
2023-05-03 17:55:09 +0200

    Update logical meter tests to use MockDataPipeline
    Signed-off-by: Sahas Subramanian <sahas.subramanian@frequenz.com>

--- tests/timeseries/test_logical_meter.py
+++ tests/timeseries/test_logical_meter.py
@@ -5,21 +5,12 @@
 
 from __future__ import annotations
 
-from datetime import datetime, timezone
-from typing import Any
-
-from frequenz.channels import Broadcast, Receiver, Sender
 from pytest_mock import MockerFixture
 
 from frequenz.sdk import microgrid
 from frequenz.sdk.microgrid.component import ComponentMetricId
-from frequenz.sdk.timeseries import Sample
 
-from ._formula_engine.utils import (
-    equal_float_lists,
-    get_resampled_stream,
-    synchronize_receivers,
-)
+from ._formula_engine.utils import equal_float_lists, get_resampled_stream
 from .mock_microgrid import MockMicrogrid
 
 # pylint: disable=too-many-locals
@@ -33,7 +24,7 @@ class TestLogicalMeter:
         mockgrid = MockMicrogrid(grid_side_meter=True)
         mockgrid.add_batteries(2)
         mockgrid.add_solar_inverters(1)
-        await mockgrid.start(mocker)
+        await mockgrid.start_mock_datapipeline(mocker)
         logical_meter = microgrid.logical_meter()
 
         grid_power_recv = logical_meter.grid_power.new_receiver()
@@ -44,12 +35,12 @@ class TestLogicalMeter:
             ComponentMetricId.ACTIVE_POWER,
         )
 
-        await synchronize_receivers([grid_power_recv, main_meter_recv])
         results = []
         main_meter_data = []
-        for _ in range(10):
+        for count in range(10):
+            await mockgrid.mock_data.send_meter_power([20.0 + count, 12.0, -13.0, -5.0])
             val = await main_meter_recv.receive()
-            assert val is not None and val.value is not None and val.value > 0.0
+            assert val is not None and val.value is not None and val.value != 0.0
             main_meter_data.append(val.value)
 
             val = await grid_power_recv.receive()
@@ -66,7 +57,7 @@ class TestLogicalMeter:
         mockgrid = MockMicrogrid(grid_side_meter=False)
         mockgrid.add_batteries(2)
         mockgrid.add_solar_inverters(1)
-        await mockgrid.start(mocker)
+        await mockgrid.start_mock_datapipeline(mocker)
         logical_meter = microgrid.logical_meter()
 
         grid_power_recv = logical_meter.grid_power.new_receiver()
@@ -80,15 +71,14 @@ class TestLogicalMeter:
             for meter_id in mockgrid.meter_ids
         ]
 
-        await synchronize_receivers([grid_power_recv, *meter_receivers])
-
         results = []
         meter_sums = []
-        for _ in range(10):
+        for count in range(10):
+            await mockgrid.mock_data.send_meter_power([20.0 + count, 12.0, -13.0, -5.0])
             meter_sum = 0.0
             for recv in meter_receivers:
                 val = await recv.receive()
-                assert val is not None and val.value is not None and val.value > 0.0
+                assert val is not None and val.value is not None and val.value != 0.0
                 meter_sum += val.value
 
             val = await grid_power_recv.receive()
@@ -109,46 +99,19 @@ class TestLogicalMeter:
         mockgrid = MockMicrogrid(grid_side_meter=False)
         mockgrid.add_batteries(2)
         mockgrid.add_solar_inverters(1)
-        await mockgrid.start(mocker)
-
-        channels: dict[int, Broadcast[Sample]] = {
-            meter_id: Broadcast(f"#{meter_id}") for meter_id in mockgrid.meter_ids
-        }
-        senders: list[Sender[Sample]] = [
-            channels[meter_id].new_sender() for meter_id in mockgrid.meter_ids
-        ]
-
-        async def send_resampled_data(
-            now: datetime,
-            meter_data: tuple[float | None, float | None, float | None, float | None],
-        ) -> None:
-            """Send resampled data to the channels."""
-            for sender, value in zip(senders, meter_data):
-                await sender.send(Sample(now, value))
-
-        def mock_resampled_receiver(
-            _1: Any, component_id: int, _2: ComponentMetricId
-        ) -> Receiver[Sample]:
-            return channels[component_id].new_receiver()
-
-        mocker.patch(
-            "frequenz.sdk.timeseries._formula_engine._resampled_formula_builder"
-            ".ResampledFormulaBuilder._get_resampled_receiver",
-            mock_resampled_receiver,
-        )
+        await mockgrid.start_mock_datapipeline(mocker)
 
         logical_meter = microgrid.logical_meter()
         grid_recv = logical_meter.grid_power.new_receiver()
         grid_production_recv = logical_meter.grid_production_power.new_receiver()
         grid_consumption_recv = logical_meter.grid_consumption_power.new_receiver()
 
-        now = datetime.now()
-        await send_resampled_data(now, (1.0, 2.0, 3.0, 4.0))
+        await mockgrid.mock_data.send_meter_power([1.0, 2.0, 3.0, 4.0])
         assert (await grid_recv.receive()).value == 10.0
         assert (await grid_production_recv.receive()).value == 0.0
         assert (await grid_consumption_recv.receive()).value == 10.0
 
-        await send_resampled_data(now, (1.0, 2.0, -3.0, -4.0))
+        await mockgrid.mock_data.send_meter_power([1.0, 2.0, -3.0, -4.0])
         assert (await grid_recv.receive()).value == -4.0
         assert (await grid_production_recv.receive()).value == 4.0
         assert (await grid_consumption_recv.receive()).value == 0.0
@@ -158,35 +121,7 @@ class TestLogicalMeter:
         mockgrid = MockMicrogrid(grid_side_meter=False)
         mockgrid.add_chps(1)
         mockgrid.add_batteries(2)
-        await mockgrid.start(mocker)
-
-        assert len(mockgrid.meter_ids) == 4
-
-        channels: dict[int, Broadcast[Sample]] = {
-            meter_id: Broadcast(f"#{meter_id}") for meter_id in [*mockgrid.meter_ids]
-        }
-        senders: list[Sender[Sample]] = [
-            channels[component_id].new_sender() for component_id in mockgrid.meter_ids
-        ]
-
-        async def send_resampled_data(
-            now: datetime,
-            meter_data: list[float | None],
-        ) -> None:
-            """Send resampled data to the channels."""
-            for sender, value in zip(senders, meter_data):
-                await sender.send(Sample(now, value))
-
-        def mock_resampled_receiver(
-            _1: Any, component_id: int, _2: ComponentMetricId
-        ) -> Receiver[Sample]:
-            return channels[component_id].new_receiver()
-
-        mocker.patch(
-            "frequenz.sdk.timeseries._formula_engine._resampled_formula_builder"
-            ".ResampledFormulaBuilder._get_resampled_receiver",
-            mock_resampled_receiver,
-        )
+        await mockgrid.start_mock_datapipeline(mocker)
 
         logical_meter = microgrid.logical_meter()
         chp_power_receiver = logical_meter.chp_power.new_receiver()
@@ -197,13 +132,12 @@ class TestLogicalMeter:
             logical_meter.chp_consumption_power.new_receiver()
         )
 
-        now = datetime.now(tz=timezone.utc)
-        await send_resampled_data(now, [1.0, 2.0, 3.0, 4.0])
+        await mockgrid.mock_data.send_meter_power([1.0, 2.0, 3.0, 4.0])
         assert (await chp_power_receiver.receive()).value == 2.0
         assert (await chp_production_power_receiver.receive()).value == 0.0
         assert (await chp_consumption_power_receiver.receive()).value == 2.0
 
-        await send_resampled_data(now, [-4.0, -12.0, None, 10.2])
+        await mockgrid.mock_data.send_meter_power([-4.0, -12.0, None, 10.2])
         assert (await chp_power_receiver.receive()).value == -12.0
         assert (await chp_production_power_receiver.receive()).value == 12.0
         assert (await chp_consumption_power_receiver.receive()).value == 0.0
@@ -212,36 +146,7 @@ class TestLogicalMeter:
         """Test the pv power formula."""
         mockgrid = MockMicrogrid(grid_side_meter=False)
         mockgrid.add_solar_inverters(2)
-        await mockgrid.start(mocker)
-
-        assert len(mockgrid.pv_inverter_ids) == 2
-
-        channels: dict[int, Broadcast[Sample]] = {
-            inv_id: Broadcast(f"#{inv_id}") for inv_id in [*mockgrid.pv_inverter_ids]
-        }
-        senders: list[Sender[Sample]] = [
-            channels[component_id].new_sender()
-            for component_id in mockgrid.pv_inverter_ids
-        ]
-
-        async def send_resampled_data(
-            now: datetime,
-            meter_data: list[float | None],
-        ) -> None:
-            """Send resampled data to the channels."""
-            for sender, value in zip(senders, meter_data):
-                await sender.send(Sample(now, value))
-
-        def mock_resampled_receiver(
-            _1: Any, component_id: int, _2: ComponentMetricId
-        ) -> Receiver[Sample]:
-            return channels[component_id].new_receiver()
-
-        mocker.patch(
-            "frequenz.sdk.timeseries._formula_engine._resampled_formula_builder"
-            ".ResampledFormulaBuilder._get_resampled_receiver",
-            mock_resampled_receiver,
-        )
+        await mockgrid.start_mock_datapipeline(mocker)
 
         logical_meter = microgrid.logical_meter()
         pv_power_receiver = logical_meter.pv_power.new_receiver()
@@ -250,8 +155,7 @@ class TestLogicalMeter:
             logical_meter.pv_consumption_power.new_receiver()
         )
 
-        now = datetime.now(tz=timezone.utc)
-        await send_resampled_data(now, [-1.0, -2.0])
+        await mockgrid.mock_data.send_meter_power([10.0, -1.0, -2.0])
         assert (await pv_power_receiver.receive()).value == -3.0
         assert (await pv_production_power_receiver.receive()).value == 3.0
         assert (await pv_consumption_power_receiver.receive()).value == 0.0
@@ -261,41 +165,12 @@ class TestLogicalMeter:
         mockgrid = MockMicrogrid(grid_side_meter=True)
         mockgrid.add_batteries(2)
         mockgrid.add_solar_inverters(2)
-        await mockgrid.start(mocker)
-
-        assert len(mockgrid.meter_ids) == 5
-
-        channels: dict[int, Broadcast[Sample]] = {
-            meter_id: Broadcast(f"#{meter_id}") for meter_id in [*mockgrid.meter_ids]
-        }
-        senders: list[Sender[Sample]] = [
-            channels[component_id].new_sender() for component_id in mockgrid.meter_ids
-        ]
-
-        async def send_resampled_data(
-            now: datetime,
-            meter_data: list[float | None],
-        ) -> None:
-            """Send resampled data to the channels."""
-            for sender, value in zip(senders, meter_data):
-                await sender.send(Sample(now, value))
-
-        def mock_resampled_receiver(
-            _1: Any, component_id: int, _2: ComponentMetricId
-        ) -> Receiver[Sample]:
-            return channels[component_id].new_receiver()
-
-        mocker.patch(
-            "frequenz.sdk.timeseries._formula_engine._resampled_formula_builder"
-            ".ResampledFormulaBuilder._get_resampled_receiver",
-            mock_resampled_receiver,
-        )
+        await mockgrid.start_mock_datapipeline(mocker)
 
         logical_meter = microgrid.logical_meter()
         consumer_power_receiver = logical_meter.consumer_power.new_receiver()
 
-        now = datetime.now(tz=timezone.utc)
-        await send_resampled_data(now, [20.0, 2.0, 3.0, 4.0, 5.0])
+        await mockgrid.mock_data.send_meter_power([20.0, 2.0, 3.0, 4.0, 5.0])
         assert (await consumer_power_receiver.receive()).value == 6.0
 
     async def test_consumer_power_no_grid_meter(self, mocker: MockerFixture) -> None:
@@ -303,39 +178,10 @@ class TestLogicalMeter:
         mockgrid = MockMicrogrid(grid_side_meter=False)
         mockgrid.add_batteries(2)
         mockgrid.add_solar_inverters(2)
-        await mockgrid.start(mocker)
-
-        assert len(mockgrid.meter_ids) == 5
-
-        channels: dict[int, Broadcast[Sample]] = {
-            meter_id: Broadcast(f"#{meter_id}") for meter_id in [*mockgrid.meter_ids]
-        }
-        senders: list[Sender[Sample]] = [
-            channels[component_id].new_sender() for component_id in mockgrid.meter_ids
-        ]
-
-        async def send_resampled_data(
-            now: datetime,
-            meter_data: list[float | None],
-        ) -> None:
-            """Send resampled data to the channels."""
-            for sender, value in zip(senders, meter_data):
-                await sender.send(Sample(now, value))
-
-        def mock_resampled_receiver(
-            _1: Any, component_id: int, _2: ComponentMetricId
-        ) -> Receiver[Sample]:
-            return channels[component_id].new_receiver()
-
-        mocker.patch(
-            "frequenz.sdk.timeseries._formula_engine._resampled_formula_builder"
-            ".ResampledFormulaBuilder._get_resampled_receiver",
-            mock_resampled_receiver,
-        )
+        await mockgrid.start_mock_datapipeline(mocker)
 
         logical_meter = microgrid.logical_meter()
         consumer_power_receiver = logical_meter.consumer_power.new_receiver()
 
-        now = datetime.now(tz=timezone.utc)
-        await send_resampled_data(now, [20.0, 2.0, 3.0, 4.0, 5.0])
+        await mockgrid.mock_data.send_meter_power([20.0, 2.0, 3.0, 4.0, 5.0])
         assert (await consumer_power_receiver.receive()).value == 20.0
