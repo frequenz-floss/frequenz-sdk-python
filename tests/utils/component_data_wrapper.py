6d4f2ad56ea360b04471778d067fd69687539fdc
Sahas Subramanian <sahas.subramanian@proton.me>
2023-07-10 16:03:48 +0200

    Rewrite component data wrapper classes to not redefine data fields
    These classes were redefining all parameters, and that had the risk of
    going out of sync with the fields in the base class.
    
    This PR ties the wrappers with their base classes, such that it is
    easy to identify changes to the base classes, without updates to the
    wrappers.
    
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- tests/utils/component_data_wrapper.py
+++ tests/utils/component_data_wrapper.py
@@ -13,7 +13,7 @@ that will need to be updated in such cases.
 from __future__ import annotations
 
 import math
-from dataclasses import dataclass, field, replace
+from dataclasses import dataclass, replace
 from datetime import datetime
 from typing import Tuple
 
@@ -30,24 +30,47 @@ from frequenz.sdk.microgrid.component import (
 )
 
 
-@dataclass(frozen=True)
 class BatteryDataWrapper(BatteryData):
     """Wrapper for the BatteryData with default arguments."""
 
-    soc: float = math.nan
-    soc_lower_bound: float = math.nan
-    soc_upper_bound: float = math.nan
-    capacity: float = math.nan
-    power_lower_bound: float = math.nan
-    power_upper_bound: float = math.nan
-    temperature_max: float = math.nan
-    _relay_state: battery_pb.RelayState.ValueType = (
-        battery_pb.RelayState.RELAY_STATE_UNSPECIFIED
-    )
-    _component_state: battery_pb.ComponentState.ValueType = (
-        battery_pb.ComponentState.COMPONENT_STATE_UNSPECIFIED
-    )
-    _errors: list[battery_pb.Error] = field(default_factory=list)
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        component_id: int,
+        timestamp: datetime,
+        soc: float = math.nan,
+        soc_lower_bound: float = math.nan,
+        soc_upper_bound: float = math.nan,
+        capacity: float = math.nan,
+        power_lower_bound: float = math.nan,
+        power_upper_bound: float = math.nan,
+        temperature_max: float = math.nan,
+        _relay_state: battery_pb.RelayState.ValueType = (
+            battery_pb.RelayState.RELAY_STATE_UNSPECIFIED
+        ),
+        _component_state: battery_pb.ComponentState.ValueType = (
+            battery_pb.ComponentState.COMPONENT_STATE_UNSPECIFIED
+        ),
+        _errors: list[battery_pb.Error] | None = None,
+    ) -> None:
+        """Initialize the BatteryDataWrapper.
+
+        This is a wrapper for the BatteryData with default arguments. The parameters are
+        documented in the BatteryData class.
+        """
+        super().__init__(
+            component_id=component_id,
+            timestamp=timestamp,
+            soc=soc,
+            soc_lower_bound=soc_lower_bound,
+            soc_upper_bound=soc_upper_bound,
+            capacity=capacity,
+            power_lower_bound=power_lower_bound,
+            power_upper_bound=power_upper_bound,
+            temperature_max=temperature_max,
+            _relay_state=_relay_state,
+            _component_state=_component_state,
+            _errors=_errors if _errors else [],
+        )
 
     def copy_with_new_timestamp(self, new_timestamp: datetime) -> BatteryDataWrapper:
         """Copy the component data but insert new timestamp.
@@ -68,13 +91,32 @@ class BatteryDataWrapper(BatteryData):
 class InverterDataWrapper(InverterData):
     """Wrapper for the InverterData with default arguments."""
 
-    active_power: float = math.nan
-    active_power_lower_bound: float = math.nan
-    active_power_upper_bound: float = math.nan
-    _component_state: inverter_pb.ComponentState.ValueType = (
-        inverter_pb.ComponentState.COMPONENT_STATE_UNSPECIFIED
-    )
-    _errors: list[inverter_pb.Error] = field(default_factory=list)
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        component_id: int,
+        timestamp: datetime,
+        active_power: float = math.nan,
+        active_power_lower_bound: float = math.nan,
+        active_power_upper_bound: float = math.nan,
+        _component_state: inverter_pb.ComponentState.ValueType = (
+            inverter_pb.ComponentState.COMPONENT_STATE_UNSPECIFIED
+        ),
+        _errors: list[inverter_pb.Error] | None = None,
+    ) -> None:
+        """Initialize the InverterDataWrapper.
+
+        This is a wrapper for the InverterData with default arguments. The parameters
+        are documented in the InverterData class.
+        """
+        super().__init__(
+            component_id=component_id,
+            timestamp=timestamp,
+            active_power=active_power,
+            active_power_lower_bound=active_power_lower_bound,
+            active_power_upper_bound=active_power_upper_bound,
+            _component_state=_component_state,
+            _errors=_errors if _errors else [],
+        )
 
     def copy_with_new_timestamp(self, new_timestamp: datetime) -> InverterDataWrapper:
         """Copy the component data but insert new timestamp.
@@ -95,15 +137,38 @@ class InverterDataWrapper(InverterData):
 class EvChargerDataWrapper(EVChargerData):
     """Wrapper for the EvChargerData with default arguments."""
 
-    active_power: float = math.nan
-    current_per_phase: Tuple[float, float, float] = field(
-        default_factory=lambda: (math.nan, math.nan, math.nan)
-    )
-    voltage_per_phase: Tuple[float, float, float] = field(
-        default_factory=lambda: (math.nan, math.nan, math.nan)
-    )
-    cable_state: EVChargerCableState = EVChargerCableState.UNSPECIFIED
-    component_state: EVChargerComponentState = EVChargerComponentState.UNSPECIFIED
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        component_id: int,
+        timestamp: datetime,
+        active_power: float = math.nan,
+        current_per_phase: Tuple[float, float, float] | None = None,
+        voltage_per_phase: Tuple[float, float, float] | None = None,
+        cable_state: EVChargerCableState = EVChargerCableState.UNSPECIFIED,
+        component_state: EVChargerComponentState = EVChargerComponentState.UNSPECIFIED,
+    ) -> None:
+        """Initialize the EvChargerDataWrapper.
+
+        This is a wrapper for the EvChargerData with default arguments. The parameters
+        are documented in the EvChargerData class.
+        """
+        super().__init__(
+            component_id=component_id,
+            timestamp=timestamp,
+            active_power=active_power,
+            current_per_phase=(
+                current_per_phase
+                if current_per_phase
+                else (math.nan, math.nan, math.nan)
+            ),
+            voltage_per_phase=(
+                voltage_per_phase
+                if voltage_per_phase
+                else (math.nan, math.nan, math.nan)
+            ),
+            cable_state=cable_state,
+            component_state=component_state,
+        )
 
     def copy_with_new_timestamp(self, new_timestamp: datetime) -> EvChargerDataWrapper:
         """Copy the component data but insert new timestamp.
@@ -124,14 +189,36 @@ class EvChargerDataWrapper(EVChargerData):
 class MeterDataWrapper(MeterData):
     """Wrapper for the MeterData with default arguments."""
 
-    active_power: float = math.nan
-    current_per_phase: Tuple[float, float, float] = field(
-        default_factory=lambda: (math.nan, math.nan, math.nan)
-    )
-    voltage_per_phase: Tuple[float, float, float] = field(
-        default_factory=lambda: (math.nan, math.nan, math.nan)
-    )
-    frequency: float = math.nan
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        component_id: int,
+        timestamp: datetime,
+        active_power: float = math.nan,
+        current_per_phase: Tuple[float, float, float] | None = None,
+        voltage_per_phase: Tuple[float, float, float] | None = None,
+        frequency: float = math.nan,
+    ) -> None:
+        """Initialize the MeterDataWrapper.
+
+        This is a wrapper for the MeterData with default arguments. The parameters are
+        documented in the MeterData class.
+        """
+        super().__init__(
+            component_id=component_id,
+            timestamp=timestamp,
+            active_power=active_power,
+            current_per_phase=(
+                current_per_phase
+                if current_per_phase
+                else (math.nan, math.nan, math.nan)
+            ),
+            voltage_per_phase=(
+                voltage_per_phase
+                if voltage_per_phase
+                else (math.nan, math.nan, math.nan)
+            ),
+            frequency=frequency,
+        )
 
     def copy_with_new_timestamp(self, new_timestamp: datetime) -> MeterDataWrapper:
         """Copy the component data but insert new timestamp.
