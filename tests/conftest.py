aa8411cccd573e65e95355a77061267cb869d391
Leandro Lucarella <luca-frequenz@llucax.com>
2023-08-21 09:30:48 +0200

    Reset the actor restart limit in the fixture
    Now that actors are stopped properly, we can reestablish the restart
    limit after it was changed.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,15 +12,10 @@ SAFETY_TIMEOUT = 10.0
 
 
 @pytest.fixture(scope="session", autouse=True)
-def disable_actor_auto_restart():  # type: ignore
-    """Disable auto-restart of actors while running tests.
-
-    At some point we had a version that would set the limit back to the
-    original value but it doesn't work because some actors will keep running
-    even after the end of the session and fail after the original value was
-    reestablished, getting into an infinite loop again.
-
-    Note: Test class must derive after unittest.IsolatedAsyncioTestCase.
-    Otherwise this fixture won't run.
-    """
-    _actor.Actor._restart_limit = 0  # pylint: disable=protected-access
+def disable_actor_auto_restart() -> collections.abc.Iterator[None]:
+    """Disable auto-restart of actors while running tests."""
+    # pylint: disable=protected-access
+    original_limit = _actor.Actor._restart_limit
+    _actor.Actor._restart_limit = 0
+    yield
+    _actor.Actor._restart_limit = original_limit
