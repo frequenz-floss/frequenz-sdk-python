a9df5de8de3afc9ca06c8eaf71205a0ca95beef9
Sahas Subramanian <sahas.subramanian@proton.me>
2023-09-16 22:04:23 +0200

    Test BatteryPool control methods with lower level streaming mocks
    This is necessary because, now that the PowerManagingActor requires
    system bounds coming from the battery pool,  we need the bounds
    calculation part of the battery pool to receive mock raw data as well.
    
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- tests/timeseries/_battery_pool/test_battery_pool_control_methods.py
+++ tests/timeseries/_battery_pool/test_battery_pool_control_methods.py
@@ -3,25 +3,81 @@
 
 """Test the battery pool control methods."""
 
+import asyncio
+import dataclasses
 import typing
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 from unittest.mock import AsyncMock, MagicMock
 
+import pytest
+from frequenz.channels import Sender
 from pytest_mock import MockerFixture
 
 from frequenz.sdk import microgrid
+from frequenz.sdk.actor import ResamplerConfig
+from frequenz.sdk.actor.power_distributing import BatteryStatus
 from frequenz.sdk.actor.power_distributing._battery_pool_status import BatteryPoolStatus
 from frequenz.sdk.timeseries import Power
 
+from ...utils.component_data_streamer import MockComponentDataStreamer
 from ...utils.component_data_wrapper import BatteryDataWrapper, InverterDataWrapper
 from ..mock_microgrid import MockMicrogrid
+
+
+@dataclasses.dataclass(frozen=True)
+class Mocks:
+    """Mocks for the tests."""
+
+    microgrid: MockMicrogrid
+    """A mock microgrid instance."""
+
+    streamer: MockComponentDataStreamer
+    """A mock component data streamer."""
+
+    battery_status_sender: Sender[BatteryStatus]
+    """Sender for sending status of the batteries."""
+
+
+# pylint doesn't understand fixtures. It thinks it is redefined name.
+# pylint: disable=redefined-outer-name
+
+
+@pytest.fixture
+async def mocks(mocker: MockerFixture) -> typing.AsyncIterator[Mocks]:
+    """Fixture for the mocks."""
+    mockgrid = MockMicrogrid()
+    mockgrid.add_batteries(4)
+    await mockgrid.start(mocker)
+
+    # pylint: disable=protected-access
+    if microgrid._data_pipeline._DATA_PIPELINE is not None:
+        microgrid._data_pipeline._DATA_PIPELINE = None
+    await microgrid._data_pipeline.initialize(
+        ResamplerConfig(resampling_period=timedelta(seconds=0.1))
+    )
+    streamer = MockComponentDataStreamer(mockgrid.mock_client)
+
+    assert microgrid._data_pipeline._DATA_PIPELINE is not None
+
+    yield Mocks(
+        mockgrid,
+        streamer,
+        microgrid._data_pipeline._DATA_PIPELINE._battery_status_channel.new_sender(),
+    )
+
+    await asyncio.gather(
+        *[
+            microgrid._data_pipeline._DATA_PIPELINE._stop(),
+            streamer.stop(),
+        ]
+    )
 
 
 class TestBatteryPoolControl:
     """Test the battery pool control methods."""
 
-    def _patch_battery_pool_status(
-        self, mocker: MockerFixture, battery_ids: list[int] | None = None
+    async def _patch_battery_pool_status(
+        self, mocks: Mocks, mocker: MockerFixture, battery_ids: list[int] | None = None
     ) -> None:
         """Patch the battery pool status.
 
@@ -42,15 +98,14 @@ class TestBatteryPoolControl:
                 "frequenz.sdk.actor.power_distributing.power_distributing.BatteryPoolStatus",
                 return_value=mock,
             )
+        await mocks.battery_status_sender.send(
+            BatteryStatus(working=set(mocks.microgrid.battery_ids), uncertain=set())
+        )
 
-        mocker.patch("asyncio.sleep", new_callable=AsyncMock)
-
-    async def _init_data_for_batteries(
-        self, mockgrid: MockMicrogrid, battery_ids: list[int]
-    ) -> None:
+    async def _init_data_for_batteries(self, mocks: Mocks) -> None:
         now = datetime.now(tz=timezone.utc)
-        for battery_id in battery_ids:
-            await mockgrid.mock_client.send(
+        for battery_id in mocks.microgrid.battery_ids:
+            mocks.streamer.start_streaming(
                 BatteryDataWrapper(
                     battery_id,
                     now,
@@ -62,15 +117,14 @@ class TestBatteryPoolControl:
                     power_inclusion_lower_bound=-1000.0,
                     power_inclusion_upper_bound=1000.0,
                     capacity=2000.0,
-                )
+                ),
+                0.05,
             )
 
-    async def _init_data_for_inverters(
-        self, mockgrid: MockMicrogrid, inverter_ids: list[int]
-    ) -> None:
+    async def _init_data_for_inverters(self, mocks: Mocks) -> None:
         now = datetime.now(tz=timezone.utc)
-        for inv_id in inverter_ids:
-            await mockgrid.mock_client.send(
+        for inv_id in mocks.microgrid.battery_inverter_ids:
+            mocks.streamer.start_streaming(
                 InverterDataWrapper(
                     inv_id,
                     now,
@@ -78,23 +132,27 @@ class TestBatteryPoolControl:
                     active_power_exclusion_upper_bound=0.0,
                     active_power_inclusion_lower_bound=-1000.0,
                     active_power_inclusion_upper_bound=1000.0,
-                )
+                ),
+                0.05,
             )
 
-    async def test_case_1(self, mocker: MockerFixture) -> None:
+    async def test_case_1(
+        self,
+        mocks: Mocks,
+        mocker: MockerFixture,
+    ) -> None:
         """Test case 1.
 
         - single battery pool with all batteries.
         - all batteries are working.
         """
-        mockgrid = MockMicrogrid()
-        mockgrid.add_batteries(4)
-        await mockgrid.start(mocker)
         set_power = typing.cast(
             AsyncMock, microgrid.connection_manager.get().api_client.set_power
         )
 
-        self._patch_battery_pool_status(mocker)
+        await self._patch_battery_pool_status(mocks, mocker)
+        await self._init_data_for_batteries(mocks)
+        await self._init_data_for_inverters(mocks)
 
         battery_pool = microgrid.battery_pool()
 
@@ -105,44 +163,39 @@ class TestBatteryPoolControl:
         # subsequent commit.
         results_rx = battery_pool.power_distribution_results()
 
-        await self._init_data_for_batteries(mockgrid, mockgrid.battery_ids)
-        await self._init_data_for_inverters(mockgrid, mockgrid.battery_inverter_ids)
-
         await battery_pool.set_power(Power.from_watts(1000.0))
         await results_rx.receive()
+
         assert set_power.call_count == 4
         assert set_power.call_args_list == [
-            mocker.call(inv_id, 250.0) for inv_id in mockgrid.battery_inverter_ids
+            mocker.call(inv_id, 250.0)
+            for inv_id in mocks.microgrid.battery_inverter_ids
         ]
-        await mockgrid.cleanup()
 
-    async def test_case_2(self, mocker: MockerFixture) -> None:
+    async def test_case_2(self, mocks: Mocks, mocker: MockerFixture) -> None:
         """Test case 2.
 
         - two battery pools with different batteries.
         - all batteries are working.
         """
-        mockgrid = MockMicrogrid()
-        mockgrid.add_batteries(4)
-        await mockgrid.start(mocker)
         set_power = typing.cast(
             AsyncMock, microgrid.connection_manager.get().api_client.set_power
         )
 
-        self._patch_battery_pool_status(mocker)
+        await self._patch_battery_pool_status(mocks, mocker)
+        await self._init_data_for_batteries(mocks)
+        await self._init_data_for_inverters(mocks)
 
-        battery_pool_1 = microgrid.battery_pool(set(mockgrid.battery_ids[:2]))
+        battery_pool_1 = microgrid.battery_pool(set(mocks.microgrid.battery_ids[:2]))
+        battery_pool_2 = microgrid.battery_pool(set(mocks.microgrid.battery_ids[2:]))
+
         results_rx = battery_pool_1.power_distribution_results()
-        battery_pool_2 = microgrid.battery_pool(set(mockgrid.battery_ids[2:]))
-
-        await self._init_data_for_batteries(mockgrid, mockgrid.battery_ids)
-        await self._init_data_for_inverters(mockgrid, mockgrid.battery_inverter_ids)
-
         await battery_pool_1.set_power(Power.from_watts(1000.0))
         await results_rx.receive()
         assert set_power.call_count == 2
         assert set_power.call_args_list == [
-            mocker.call(inv_id, 500.0) for inv_id in mockgrid.battery_inverter_ids[:2]
+            mocker.call(inv_id, 500.0)
+            for inv_id in mocks.microgrid.battery_inverter_ids[:2]
         ]
         set_power.reset_mock()
 
@@ -150,32 +203,28 @@ class TestBatteryPoolControl:
         await results_rx.receive()
         assert set_power.call_count == 2
         assert set_power.call_args_list == [
-            mocker.call(inv_id, 500.0) for inv_id in mockgrid.battery_inverter_ids[2:]
+            mocker.call(inv_id, 500.0)
+            for inv_id in mocks.microgrid.battery_inverter_ids[2:]
         ]
-        await mockgrid.cleanup()
 
-    async def test_case_3(self, mocker: MockerFixture) -> None:
+    async def test_case_3(self, mocks: Mocks, mocker: MockerFixture) -> None:
         """Test case 3.
 
         - two battery pools with same batteries, but different priorities.
         - all batteries are working.
         """
-        mockgrid = MockMicrogrid()
-        mockgrid.add_batteries(4)
-        await mockgrid.start(mocker)
         set_power = typing.cast(
             AsyncMock, microgrid.connection_manager.get().api_client.set_power
         )
 
-        self._patch_battery_pool_status(mocker)
+        await self._patch_battery_pool_status(mocks, mocker)
+        await self._init_data_for_batteries(mocks)
+        await self._init_data_for_inverters(mocks)
 
         battery_pool_1 = microgrid.battery_pool()
-        results_rx = battery_pool_1.power_distribution_results()
         battery_pool_2 = microgrid.battery_pool()
 
-        await self._init_data_for_batteries(mockgrid, mockgrid.battery_ids)
-        await self._init_data_for_inverters(mockgrid, mockgrid.battery_inverter_ids)
-
+        results_rx = battery_pool_1.power_distribution_results()
         await battery_pool_1.set_power(
             Power.from_watts(-1000.0),
             _priority=2,
@@ -184,7 +233,8 @@ class TestBatteryPoolControl:
         await results_rx.receive()
         assert set_power.call_count == 4
         assert set_power.call_args_list == [
-            mocker.call(inv_id, -250.0) for inv_id in mockgrid.battery_inverter_ids
+            mocker.call(inv_id, -250.0)
+            for inv_id in mocks.microgrid.battery_inverter_ids
         ]
         set_power.reset_mock()
 
@@ -196,6 +246,5 @@ class TestBatteryPoolControl:
         await results_rx.receive()
         assert set_power.call_count == 4
         assert set_power.call_args_list == [
-            mocker.call(inv_id, 0.0) for inv_id in mockgrid.battery_inverter_ids
+            mocker.call(inv_id, 0.0) for inv_id in mocks.microgrid.battery_inverter_ids
         ]
-        await mockgrid.cleanup()
