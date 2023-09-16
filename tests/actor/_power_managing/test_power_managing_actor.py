71fe578128de5279f0b0d113704d24c0cb37e708
Sahas Subramanian <sahas.subramanian@proton.me>
2023-09-16 18:59:44 +0200

    Mock `_add_bounds_tracker` in the tests for the PowerManagingActor
    And use a mock `_system_bounds` value to use with the test.
    
    The tests for the PowerManagingActor are really just testing the
    algorithm and don't use the MockMicrogrid, and `_add_bounds_tracker`
    will not be able to track system bounds, causing the tests to fail.
    
    Higher level tests using the MockMicrogrid are done as part of the
    tests for the control interface of the BatteryPool.
    
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- tests/actor/_power_managing/test_power_managing_actor.py
+++ tests/actor/_power_managing/test_power_managing_actor.py
@@ -3,15 +3,19 @@
 
 """Tests for the power managing actor."""
 
+from datetime import datetime, timezone
+from unittest.mock import AsyncMock
+
 from frequenz.channels import Broadcast
+from pytest_mock import MockerFixture
 
 from frequenz.sdk.actor._power_managing._base_classes import Proposal
 from frequenz.sdk.actor._power_managing._power_managing_actor import PowerManagingActor
 from frequenz.sdk.actor.power_distributing.request import Request
-from frequenz.sdk.timeseries import Power
+from frequenz.sdk.timeseries import Power, battery_pool
 
 
-async def test_power_managing_actor_matryoshka() -> None:
+async def test_power_managing_actor_matryoshka(mocker: MockerFixture) -> None:
     """Tests for the power managing actor."""
     input_channel = Broadcast[Proposal]("power managing proposals")
     output_channel = Broadcast[Request]("Power managing outputs")
@@ -19,6 +23,12 @@ async def test_power_managing_actor_matryoshka() -> None:
     output_rx = output_channel.new_receiver()
 
     batteries = frozenset({2, 5})
+
+    mocker.patch(
+        "frequenz.sdk.actor._power_managing._power_managing_actor"
+        ".PowerManagingActor._add_bounds_tracker",
+        new_callable=AsyncMock,
+    )
 
     async def case(
         *,
@@ -40,7 +50,19 @@ async def test_power_managing_actor_matryoshka() -> None:
 
     async with PowerManagingActor(
         input_channel.new_receiver(), output_channel.new_sender()
-    ):
+    ) as powmgract:
+        powmgract._system_bounds[  # pylint: disable=protected-access
+            batteries
+        ] = battery_pool.PowerMetrics(
+            timestamp=datetime.now(tz=timezone.utc),
+            inclusion_bounds=battery_pool.Bounds(
+                lower=Power.from_watts(-200.0), upper=Power.from_watts(200.0)
+            ),
+            exclusion_bounds=battery_pool.Bounds(
+                lower=Power.zero(), upper=Power.zero()
+            ),
+        )
+
         await case(priority=2, power=25.0, bounds=(25.0, 50.0), expected=25.0)
         await case(priority=1, power=20.0, bounds=(20.0, 50.0), expected=25.0)
         await case(priority=3, power=10.0, bounds=(10.0, 15.0), expected=10.0)
