46704a4570152ef3750de2613d13c9702cae43df
Sahas Subramanian <sahas.subramanian@proton.me>
2023-10-19 15:13:13 +0200

    Improve docs for the `FormulaEngine` class
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- src/frequenz/sdk/timeseries/formula_engine/__init__.py
+++ src/frequenz/sdk/timeseries/formula_engine/__init__.py
@@ -1,7 +1,13 @@
 # License: MIT
 # Copyright © 2023 Frequenz Energy-as-a-Service GmbH
 
-"""A formula engine for applying formulas."""
+"""The formula engine module.
+
+This module exposes the
+[FormulaEngine][frequenz.sdk.timeseries.formula_engine.FormulaEngine] and
+[FormulaEngine3Phase][frequenz.sdk.timeseries.formula_engine.FormulaEngine3Phase]
+classes.
+"""
 
 from ._formula_engine import FormulaEngine, FormulaEngine3Phase
 
