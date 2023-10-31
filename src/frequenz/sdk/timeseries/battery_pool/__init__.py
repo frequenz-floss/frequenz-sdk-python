11f325c6bd83d8491f582bf2fd88a6f8cc63a1f9
Sahas Subramanian <sahas.subramanian@proton.me>
2023-10-31 16:17:24 +0100

    Expose `PowerManagingActor`'s `Report` type through the `BatteryPool`
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- src/frequenz/sdk/timeseries/battery_pool/__init__.py
+++ src/frequenz/sdk/timeseries/battery_pool/__init__.py
@@ -3,10 +3,12 @@
 
 """Manage a pool of batteries."""
 
+from ...actor._power_managing import Report
 from ._battery_pool import BatteryPool
 from ._result_types import PowerMetrics
 
 __all__ = [
     "BatteryPool",
     "PowerMetrics",
+    "Report",
 ]
