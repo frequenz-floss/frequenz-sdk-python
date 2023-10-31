b518af54883a264f392d166b0b6797467e6aa3be
Sahas Subramanian <sahas.subramanian@proton.me>
2023-10-31 18:36:13 +0100

    Add method for adjusting power values to battery pool's reports
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- src/frequenz/sdk/actor/_power_managing/_base_classes.py
+++ src/frequenz/sdk/actor/_power_managing/_base_classes.py
@@ -13,6 +13,7 @@ import typing
 
 from ... import timeseries
 from ...timeseries import Power
+from ._bounds_methods import _clamp_to_bounds
 
 if typing.TYPE_CHECKING:
     from ...timeseries.battery_pool import PowerMetrics
@@ -71,6 +72,66 @@ class Report:
 
     This is `None` if no power distribution has been performed yet.
     """
+
+    def adjust_to_bounds(self, power: Power) -> tuple[Power | None, Power | None]:
+        """Adjust a power value to the bounds.
+
+        This method can be used to adjust a desired power value to the power bounds
+        available to the actor.
+
+        If the given power value falls within the usable bounds, it will be returned
+        unchanged.
+
+        If it falls outside the usable bounds, the closest possible value on the
+        corresponding side will be returned.  For example, if the given power is lower
+        than the lowest usable power, only the lowest usable power will be returned, and
+        similarly for the highest usable power.
+
+        If the given power falls within an exclusion zone that's contained within the
+        usable bounds, the closest possible power values on both sides will be returned.
+
+        !!! note
+            It is completely optional to use this method to adjust power values before
+            proposing them through the battery pool, because the battery pool will do
+            this automatically.  This method is provided for convenience, and for
+            granular control when there are two possible power values, both of which
+            fall within the available bounds.
+
+        Example:
+            ```python
+            from frequenz.sdk import microgrid
+
+            power_status_rx = microgrid.battery_pool().power_status.new_receiver()
+            power_status = await power_status_rx.receive()
+            desired_power = Power.from_watts(1000.0)
+
+            match power_status.adjust_to_bounds(desired_power):
+                case (power, _) if power == desired_power:
+                    print("Desired power is available.")
+                case (None, power) | (power, None) if power:
+                    print(f"Closest available power is {power}.")
+                case (lower, upper) if lower and upper:
+                    print(f"Two options {lower}, {upper} to propose to battery pool.")
+                case (None, None):
+                    print("No available power")
+            ```
+
+        Args:
+            power: The power value to adjust.
+
+        Returns:
+            A tuple of the closest power values to the desired power that fall within
+                the available bounds for the actor.
+        """
+        if self.inclusion_bounds is None:
+            return None, None
+
+        return _clamp_to_bounds(
+            power,
+            self.inclusion_bounds.lower,
+            self.inclusion_bounds.upper,
+            self.exclusion_bounds,
+        )
 
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
---
+++ tests/actor/_power_managing/test_report.py
@@ -0,0 +1,119 @@
+# License: MIT
+# Copyright © 2023 Frequenz Energy-as-a-Service GmbH
+
+"""Tests for methods provided by the PowerManager's reports."""
+
+from frequenz.sdk.actor._power_managing import Report
+from frequenz.sdk.timeseries import Bounds, Power
+
+
+class BoundsTester:
+    """Test the bounds adjustment."""
+
+    def __init__(
+        self,
+        inclusion_bounds: tuple[float, float] | None,
+        exclusion_bounds: tuple[float, float] | None,
+    ) -> None:
+        """Initialize the tester."""
+        self._report = Report(
+            target_power=None,
+            inclusion_bounds=Bounds(
+                Power.from_watts(inclusion_bounds[0]),
+                Power.from_watts(inclusion_bounds[1]),
+            )
+            if inclusion_bounds is not None
+            else None,
+            exclusion_bounds=Bounds(
+                Power.from_watts(exclusion_bounds[0]),
+                Power.from_watts(exclusion_bounds[1]),
+            )
+            if exclusion_bounds is not None
+            else None,
+            distribution_result=None,
+        )
+
+    def case(
+        self,
+        desired_power: float,
+        expected_lower: float | None,
+        expected_upper: float | None,
+    ) -> None:
+        """Test a case."""
+        lower, upper = self._report.adjust_to_bounds(Power.from_watts(desired_power))
+
+        tgt_lower = (
+            Power.from_watts(expected_lower) if expected_lower is not None else None
+        )
+        assert lower == tgt_lower
+        tgt_upper = (
+            Power.from_watts(expected_upper) if expected_upper is not None else None
+        )
+        assert upper == tgt_upper
+
+
+def test_adjust_to_bounds() -> None:
+    """Test that desired powers are adjusted to bounds correctly."""
+    tester = BoundsTester(
+        inclusion_bounds=(-200.0, 200.0),
+        exclusion_bounds=(-30.0, 30.0),
+    )
+    tester.case(0.0, -30.0, 30.0)
+    tester.case(-210.0, -200.0, None)
+    tester.case(220.0, None, 200.0)
+    tester.case(-20.0, -30.0, 30.0)
+    tester.case(-30.0, -30.0, -30.0)
+    tester.case(30.0, 30.0, 30.0)
+    tester.case(50.0, 50.0, 50.0)
+    tester.case(-200.0, -200.0, -200.0)
+    tester.case(200.0, 200.0, 200.0)
+
+    tester = BoundsTester(
+        inclusion_bounds=(-200.0, 200.0),
+        exclusion_bounds=(-210.0, 0.0),
+    )
+    tester.case(0.0, 0.0, 0.0)
+    tester.case(-210.0, None, 0.0)
+    tester.case(220.0, None, 200.0)
+    tester.case(-20.0, None, 0.0)
+    tester.case(20.0, 20.0, 20.0)
+
+    tester = BoundsTester(
+        inclusion_bounds=(-200.0, 200.0),
+        exclusion_bounds=(0.0, 210.0),
+    )
+    tester.case(0.0, 0.0, 0.0)
+    tester.case(-210.0, -200.0, None)
+    tester.case(220.0, 0.0, None)
+    tester.case(-20.0, -20.0, -20.0)
+    tester.case(20.0, 0.0, None)
+
+    tester = BoundsTester(
+        inclusion_bounds=(-200.0, 200.0),
+        exclusion_bounds=None,
+    )
+    tester.case(0.0, 0.0, 0.0)
+    tester.case(-210.0, -200.0, None)
+    tester.case(220.0, None, 200.0)
+    tester.case(-20.0, -20.0, -20.0)
+    tester.case(20.0, 20.0, 20.0)
+
+    tester = BoundsTester(
+        inclusion_bounds=(-200.0, 200.0),
+        exclusion_bounds=(-210.0, 210.0),
+    )
+    tester.case(0.0, None, None)
+    tester.case(-210.0, None, None)
+    tester.case(220.0, None, None)
+    tester.case(-20.0, None, None)
+    tester.case(20.0, None, None)
+
+    tester = BoundsTester(
+        inclusion_bounds=None,
+        exclusion_bounds=(-210.0, 210.0),
+    )
+    tester.case(0.0, None, None)
+    tester.case(-210.0, None, None)
+    tester.case(220.0, None, None)
+    tester.case(-20.0, None, None)
+    tester.case(20.0, None, None)
