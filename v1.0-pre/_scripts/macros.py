e6d3ae4b247c9c914d7e79a552cc161cfd9d6825
Leandro Lucarella <luca-frequenz@llucax.com>
2023-11-03 14:02:07 +0100

    Deployed e04ee952 to v1.0-pre with MkDocs 1.5.3 and mike 2.0.0

---
+++ v1.0-pre/_scripts/macros.py
@@ -0,0 +1,87 @@
+# License: MIT
+# Copyright © 2023 Frequenz Energy-as-a-Service GmbH
+
+"""This module defines macros for use in Markdown files."""
+
+import pathlib
+from typing import Any
+
+import markdown as md
+from markdown.extensions import toc
+from mkdocs_macros import plugin as macros
+
+_CODE_ANNOTATION_MARKER: str = (
+    r'<span class="md-annotation">'
+    r'<span class="md-annotation__index" tabindex="-1">'
+    r'<span data-md-annotation-id="1"></span>'
+    r"</span>"
+    r"</span>"
+)
+
+
+def _slugify(text: str) -> str:
+    """Slugify a text.
+
+    Args:
+        text: The text to slugify.
+
+    Returns:
+        The slugified text.
+    """
+    # The type of the return value is not defined for the markdown library.
+    # Also for some reason `mypy` thinks the `toc` module doesn't have a
+    # `slugify_unicode` function, but it definitely does.
+    return toc.slugify_unicode(text, "-")  # type: ignore[attr-defined,no-any-return]
+
+
+def define_env(env: macros.MacrosPlugin) -> None:
+    """Define the hook to create macro functions for use in Markdown.
+
+    Args:
+        env: The environment to define the macro functions in.
+    """
+    # A variable to easily show an example code annotation from mkdocs-material.
+    # https://squidfunk.github.io/mkdocs-material/reference/code-blocks/#adding-annotations
+    env.variables["code_annotation_marker"] = _CODE_ANNOTATION_MARKER
+
+    @env.macro  # type: ignore[misc]
+    def glossary(term: str) -> str:
+        """Create a link to the glossary entry for the given term.
+
+        Args:
+            term: The term to link to.
+
+        Returns:
+            The Markdown link to the glossary entry for the given term.
+        """
+        current_path = pathlib.Path(env.page.file.src_uri)
+        glossary_path = pathlib.Path("user-guide/glossary.md")
+        link_path = glossary_path.relative_to(current_path.parent)
+        return f"[{term}]({link_path}#{_slugify(term)})"
+
+    # The code below is a temporary workaround to make `mkdocs-macros` work with
+    # `mkdocstrings` until a proper `mkdocs-macros` *pluglet* is available. See
+    # https://github.com/mkdocstrings/mkdocstrings/issues/615 for details.
+
+    # get mkdocstrings' Python handler
+    python_handler = env.conf["plugins"]["mkdocstrings"].get_handler("python")
+
+    # get the `update_env` method of the Python handler
+    update_env = python_handler.update_env
+
+    # override the `update_env` method of the Python handler
+    def patched_update_env(markdown: md.Markdown, config: dict[str, Any]) -> None:
+        update_env(markdown, config)
+
+        # get the `convert_markdown` filter of the env
+        convert_markdown = python_handler.env.filters["convert_markdown"]
+
+        # build a chimera made of macros+mkdocstrings
+        def render_convert(markdown: str, *args: Any, **kwargs: Any) -> Any:
+            return convert_markdown(env.render(markdown), *args, **kwargs)
+
+        # patch the filter
+        python_handler.env.filters["convert_markdown"] = render_convert
+
+    # patch the method
+    python_handler.update_env = patched_update_env
