35f4dcef8ca4f09057e994b2cc01c7bdc5e812c8
Leandro Lucarella <luca-frequenz@llucax.com>
2024-01-11 13:09:34 +0100

    Convert `ResamplingFunction` to a `Protocol`
    Using a type alias forces us to use strings to specify types because
    Python can't use forward declarations for statements.
    
    Also using a protocol allows for more control, as even when we don't
    use it for now, it will also check function argument names if `/` is not
    used.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/frequenz/sdk/timeseries/_resampling/_config.py
+++ src/frequenz/sdk/timeseries/_resampling/_config.py
@@ -6,9 +6,10 @@
 from __future__ import annotations
 
 import logging
-from collections.abc import Callable, Sequence
+from collections.abc import Sequence
 from dataclasses import dataclass
 from datetime import datetime, timedelta
+from typing import Protocol
 
 from frequenz.quantities import Quantity
 
@@ -42,32 +43,41 @@ If a buffer length would get bigger than this, a warning will be logged.
 """
 
 
-ResamplingFunction = Callable[
-    [Sequence[Sample[Quantity]], "ResamplerConfig", "SourceProperties"], float
-]
-"""Resampling function type.
+class ResamplingFunction(Protocol):
+    """Combine multiple samples into a new one.
 
-A resampling function produces a new sample based on a list of pre-existing
-samples. It can do "upsampling" when the data rate of the `input_samples`
-period is smaller than the `resampling_period`, or "downsampling" if it is
-bigger.
+    A resampling function produces a new sample based on a list of pre-existing
+    samples. It can do "upsampling" when the data rate of the `input_samples`
+    period is smaller than the `resampling_period`, or "downsampling" if it is
+    bigger.
 
-In general a resampling window is the same as the `resampling_period`, and
-this function might receive input samples from multiple windows in the past to
-enable extrapolation, but no samples from the future (so the timestamp of the
-new sample that is going to be produced will always be bigger than the biggest
-timestamp in the input data).
+    In general, a resampling window is the same as the `resampling_period`, and
+    this function might receive input samples from multiple windows in the past to
+    enable extrapolation, but no samples from the future (so the timestamp of the
+    new sample that is going to be produced will always be bigger than the biggest
+    timestamp in the input data).
+    """
 
-Args:
-    input_samples (Sequence[Sample]): The sequence of pre-existing samples.
-    resampler_config (ResamplerConfig): The configuration of the resampling
-        calling this function.
-    source_properties (SourceProperties): The properties of the source being
-        resampled.
+    def __call__(
+        self,
+        input_samples: Sequence[Sample[Quantity]],
+        resampler_config: ResamplerConfig,
+        source_properties: SourceProperties,
+        /,
+    ) -> float:
+        """Call the resampling function.
 
-Returns:
-    new_sample (float): The value of new sample produced after the resampling.
-"""
+        Args:
+            input_samples: The sequence of pre-existing samples. It must be
+                non-empty.
+            resampler_config: The configuration of the resampler calling this
+                function.
+            source_properties: The properties of the source being resampled.
+
+        Returns:
+            The value of new sample produced after the resampling.
+        """
+        ...  # pylint: disable=unnecessary-ellipsis
 
 
 # pylint: disable=unused-argument
