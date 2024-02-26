422d7f13cfcd93fea1638ef0a4e502672a225ad3
Sahas Subramanian <sahas.subramanian@proton.me>
2024-02-26 11:33:00 +0100

    Add a system bounds tracker for EV charger pools
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

---
+++ src/frequenz/sdk/timeseries/ev_charger_pool/_system_bounds_tracker.py
@@ -0,0 +1,126 @@
+# License: MIT
+# Copyright © 2022 Frequenz Energy-as-a-Service GmbH
+
+"""System bounds tracker for the EV chargers."""
+
+
+import asyncio
+from collections import abc
+
+from frequenz.channels import Receiver, Sender
+from frequenz.channels.util import Merge, select, selected_from
+
+from ... import microgrid
+from ...actor import BackgroundService
+from ...actor.power_distributing._component_status import ComponentPoolStatus
+from .. import Power
+from .._base_types import Bounds, SystemBounds
+
+
+class EVCSystemBoundsTracker(BackgroundService):
+    """Track the system bounds for the EV chargers."""
+
+    def __init__(
+        self,
+        component_ids: abc.Set[int],
+        status_receiver: Receiver[ComponentPoolStatus],
+        bounds_sender: Sender[SystemBounds],
+    ):
+        """Initialize the system bounds tracker.
+
+        Args:
+            component_ids: The ids of the components to track.
+            status_receiver: A receiver that streams the status of the EV Chargers in
+                the pool.
+            bounds_sender: A sender to send the system bounds to.
+        """
+        super().__init__()
+
+        self._component_ids = component_ids
+        self._status_receiver = status_receiver
+        self._bounds_sender = bounds_sender
+        self._latest_component_data: dict[int, microgrid.component.EVChargerData] = {}
+        self._last_sent_bounds: SystemBounds | None = None
+
+    def start(self) -> None:
+        """Start the EV charger system bounds tracker."""
+        self._tasks.add(asyncio.create_task(self._run()))
+
+    async def _send_bounds(self) -> None:
+        inclusion_bounds = Bounds(
+            lower=Power.from_watts(
+                sum(
+                    data.active_power_inclusion_lower_bound
+                    for data in self._latest_component_data.values()
+                )
+            ),
+            upper=Power.from_watts(
+                sum(
+                    data.active_power_inclusion_upper_bound
+                    for data in self._latest_component_data.values()
+                )
+            ),
+        )
+        exclusion_bounds = Bounds(
+            lower=Power.from_watts(
+                sum(
+                    data.active_power_exclusion_lower_bound
+                    for data in self._latest_component_data.values()
+                )
+            ),
+            upper=Power.from_watts(
+                sum(
+                    data.active_power_exclusion_upper_bound
+                    for data in self._latest_component_data.values()
+                )
+            ),
+        )
+
+        if (
+            self._last_sent_bounds is None
+            or inclusion_bounds != self._last_sent_bounds.inclusion_bounds
+            or exclusion_bounds != self._last_sent_bounds.exclusion_bounds
+        ):
+            self._last_sent_bounds = SystemBounds(
+                timestamp=max(
+                    msg.timestamp for msg in self._latest_component_data.values()
+                ),
+                inclusion_bounds=inclusion_bounds,
+                exclusion_bounds=exclusion_bounds,
+            )
+            await self._bounds_sender.send(self._last_sent_bounds)
+
+    async def _run(self) -> None:
+        """Run the system bounds tracker."""
+        api_client = microgrid.connection_manager.get().api_client
+        status_rx = self._status_receiver
+        ev_data_rx = Merge(
+            *(
+                await asyncio.gather(
+                    *[api_client.ev_charger_data(cid) for cid in self._component_ids]
+                )
+            )
+        )
+
+        component_pool_status = ComponentPoolStatus(set(), set())
+
+        async for selected in select(status_rx, ev_data_rx):
+            if selected_from(selected, status_rx):
+                status = selected.value
+                component_pool_status = status
+                for comp_id in self._latest_component_data:
+                    if (
+                        comp_id not in component_pool_status.working
+                        and comp_id not in component_pool_status.uncertain
+                    ):
+                        self._latest_component_data.pop(comp_id, None)
+            elif selected_from(selected, ev_data_rx):
+                if (
+                    comp_id not in component_pool_status.working
+                    and comp_id not in component_pool_status.uncertain
+                ):
+                    continue
+                data = selected.value
+                self._latest_component_data[data.component_id] = data
+
+            await self._send_bounds()
