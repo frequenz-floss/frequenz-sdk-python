7bf62716d08c4dbe444f5ba530ecb6ab70ce8dcd
Sahas Subramanian <sahas.subramanian@proton.me>
2024-02-27 12:55:29 +0100

    Move reusable parts of EVChargerPool to EVChargerPoolReferenceStore
    This allows the efficient creation of multiple `EVChargerPool`
    instances for the same set of EV chargers, but with different
    priorities, while reusing the formulas and bounds by having only a
    single instance of the `EVChargerPoolReferenceStore` class for any
    given set of EV chargers.
    
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- src/frequenz/sdk/timeseries/ev_charger_pool/__init__.py
+++ src/frequenz/sdk/timeseries/ev_charger_pool/__init__.py
@@ -4,10 +4,8 @@
 """Interactions with EV Chargers."""
 
 from ._ev_charger_pool import EVChargerPool, EVChargerPoolError
-from ._set_current_bounds import ComponentCurrentLimit
 
 __all__ = [
-    "ComponentCurrentLimit",
     "EVChargerPool",
     "EVChargerPoolError",
 ]
