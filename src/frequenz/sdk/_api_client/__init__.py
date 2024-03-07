58a25cf4c591589fefcee0f8f15ace63998bf120
Leandro Lucarella <luca-frequenz@llucax.com>
2024-03-07 15:03:49 +0100

    Remove old and unused `_api_client` module
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/frequenz/sdk/_api_client/__init__.py
+++
@@ -1,8 +0,0 @@
-# License: MIT
-# Copyright © 2022 Frequenz Energy-as-a-Service GmbH
-
-"""Common items to be shared across all API clients."""
-
-from .api_client import ApiClient, ApiProtocol
-
-__all__ = ["ApiClient", "ApiProtocol"]
