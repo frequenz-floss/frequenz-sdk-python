a68a42ffbabf464920a71568ee802ad61aa979fc
Sahas Subramanian <sahas.subramanian@proton.me>
2024-03-28 14:36:42 +0100

    fixup! Add `power_status` method to `EVChargerPool`
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- src/frequenz/sdk/timeseries/ev_charger_pool/_result_types.py
+++ src/frequenz/sdk/timeseries/ev_charger_pool/_result_types.py
@@ -14,7 +14,7 @@ class EVChargerPoolReport(typing.Protocol):
     """A status report for an EV chargers pool."""
 
     target_power: Power | None
-    """The currently set power for the batteries."""
+    """The currently set power for the EV chargers."""
 
     distribution_result: power_distributing.Result | None
     """The result of the last power distribution.
@@ -24,13 +24,8 @@ class EVChargerPoolReport(typing.Protocol):
 
     @property
     def bounds(self) -> Bounds[Power] | None:
-        """The usable bounds for the batteries.
+        """The usable bounds for the EV chargers.
 
         These bounds are adjusted to any restrictions placed by actors with higher
         priorities.
-
-        There might be exclusion zones within these bounds. If necessary, the
-        [`adjust_to_bounds`][frequenz.sdk.timeseries.battery_pool.BatteryPoolReport.adjust_to_bounds]
-        method may be used to check if a desired power value fits the bounds, or to get
-        the closest possible power values that do fit the bounds.
         """
