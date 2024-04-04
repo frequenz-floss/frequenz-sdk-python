6be3ac483b19f49903ac2a897ae590dae7b2bb18
Sahas Subramanian <sahas.subramanian@proton.me>
2024-04-04 18:52:50 +0200

    Add a `PVManager` implementation
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- src/frequenz/sdk/actor/power_distributing/_component_managers/__init__.py
+++ src/frequenz/sdk/actor/power_distributing/_component_managers/__init__.py
@@ -6,9 +6,11 @@
 from ._battery_manager import BatteryManager
 from ._component_manager import ComponentManager
 from ._ev_charger_manager import EVChargerManager
+from ._pv_inverter_manager import PVManager
 
 __all__ = [
     "BatteryManager",
     "ComponentManager",
     "EVChargerManager",
+    "PVManager",
 ]
---
+++ src/frequenz/sdk/actor/power_distributing/_component_managers/_pv_inverter_manager/__init__.py
@@ -0,0 +1,8 @@
+# License: MIT
+# Copyright © 2024 Frequenz Energy-as-a-Service GmbH
+
+"""Manage PV inverters for the power distributor."""
+
+from ._pv_inverter_manager import PVManager
+
+__all__ = ["PVManager"]
