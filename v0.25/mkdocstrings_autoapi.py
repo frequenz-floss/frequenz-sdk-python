95cd8eec379a4e3775b76d8005784a4db28a86a4
Leandro Lucarella <luca-frequenz@llucax.com>
2024-11-20 14:20:42 +0100

    Removed v0.25 with mike 2.1.3

--- v0.25/mkdocstrings_autoapi.py
+++
@@ -1,8 +0,0 @@
-# License: MIT
-# Copyright © 2022 Frequenz Energy-as-a-Service GmbH
-
-"""Generate the code reference pages."""
-
-from frequenz.repo.config import mkdocs
-
-mkdocs.generate_python_api_pages("src", "reference")
