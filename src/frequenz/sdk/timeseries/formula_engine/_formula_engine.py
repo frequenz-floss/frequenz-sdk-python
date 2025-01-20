fafe79a3f738cbeacf26357b6aa189248a70a436
Sahas Subramanian <sahas.subramanian@proton.me>
2025-01-20 15:44:31 +0100

    Close resampled data receivers when closing formula engines
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- src/frequenz/sdk/timeseries/formula_engine/_formula_engine.py
+++ src/frequenz/sdk/timeseries/formula_engine/_formula_engine.py
@@ -117,6 +117,9 @@ class FormulaEngine(Generic[QuantityT]):
         if self._task is None:
             return
         await cancel_and_await(self._task)
+        _, fetchers = self._builder.finalize()
+        for fetcher in fetchers.values():
+            fetcher.stream.close()
 
     @classmethod
     def from_receiver(
