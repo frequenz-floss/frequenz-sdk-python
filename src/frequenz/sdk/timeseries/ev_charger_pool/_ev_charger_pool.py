058aac7ac36005a34b634a2e9b96bd37dcb320e5
Sahas Subramanian <sahas.subramanian@proton.me>
2025-04-15 18:17:52 +0200

    Don't stop pools
    When there are multiple pool instances controlling the same components
    and one of them stops, that would stop all the pools.
    
    This commit adds an emergency fix to prevent this bug, to buy some
    time to come up with a better solution.
    
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- src/frequenz/sdk/timeseries/ev_charger_pool/_ev_charger_pool.py
+++ src/frequenz/sdk/timeseries/ev_charger_pool/_ev_charger_pool.py
@@ -217,7 +217,12 @@ class EVChargerPool:
 
     async def stop(self) -> None:
         """Stop all tasks and channels owned by the EVChargerPool."""
-        await self._pool_ref_store.stop()
+        # This was closing the pool_ref_store, which is not correct, because those are
+        # shared.
+        #
+        # This method will do until we have a mechanism to track the resources created
+        # through it.  It can also eventually cleanup the pool_ref_store, when it is
+        # holding the last reference to it.
 
     @property
     def _system_power_bounds(self) -> ReceiverFetcher[SystemBounds]:
--- src/frequenz/sdk/timeseries/pv_pool/_pv_pool.py
+++ src/frequenz/sdk/timeseries/pv_pool/_pv_pool.py
@@ -179,7 +179,12 @@ class PVPool:
 
     async def stop(self) -> None:
         """Stop all tasks and channels owned by the PVPool."""
-        await self._pool_ref_store.stop()
+        # This was closing the pool_ref_store, which is not correct, because those are
+        # shared.
+        #
+        # This method will do until we have a mechanism to track the resources created
+        # through it.  It can also eventually cleanup the pool_ref_store, when it is
+        # holding the last reference to it.
 
     @property
     def _system_power_bounds(self) -> ReceiverFetcher[SystemBounds]:
