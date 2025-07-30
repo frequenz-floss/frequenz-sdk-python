9d9efe03ab5c867b0ffdf9b8df8fbba01ea38215
Leandro Lucarella <luca-frequenz@llucax.com>
2025-07-30 15:30:57 +0200

    Add a fixture to monitor `asyncio.sleep()` calls
    We monitor only the calls to sleep inside the wall clock time module,
    so we can assert those calls independently from what happens in the
    tests themselves, for example.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- tests/timeseries/_resampling/wall_clock_timer/conftest.py
+++ tests/timeseries/_resampling/wall_clock_timer/conftest.py
@@ -3,9 +3,10 @@
 
 """Fixtures for wall clock timer tests."""
 
+import asyncio
 from collections.abc import Iterator
 from datetime import datetime
-from unittest.mock import MagicMock, patch
+from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
 from frequenz.core.datetime import UNIX_EPOCH
@@ -19,3 +20,16 @@ def datetime_mock() -> Iterator[MagicMock]:
     dt_mock.now.return_value = UNIX_EPOCH
     with patch(dt_symbol, new=dt_mock):
         yield dt_mock
+
+
+@pytest.fixture
+def asyncio_sleep_mock() -> Iterator[AsyncMock]:
+    """Mock asyncio.sleep in the target module for all tests."""
+    asyncio_symbol = "frequenz.sdk.timeseries._resampling._wall_clock_timer.asyncio"
+    mock = AsyncMock(
+        name="asyncio_sleep_mock", wraps=asyncio.sleep, spec_set=asyncio.sleep
+    )
+    asyncio_mock = AsyncMock(name="asyncio_mock", wraps=asyncio, spec_set=asyncio)
+    asyncio_mock.sleep = mock
+    with patch(asyncio_symbol, new=asyncio_mock):
+        yield mock
